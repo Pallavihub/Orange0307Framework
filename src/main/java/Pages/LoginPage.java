a995d55061be6242cc33dfba532ca012e6848963
user <user@LAPTOP-6I041NL7>
2025-07-03 12:42:12 +0530

    Added test cases for home page by Arvind Tester

--- src/main/java/Pages/LoginPage.java
+++ src/main/java/Pages/LoginPage.java
@@ -2,7 +2,7 @@ package Pages;
 
 public class LoginPage {
 	
-	public static void loginFunctionality(String username,String password) {
+	public void loginFunctionality(String username,String password) {
 		
 		System.out.println("User logged into application");
 	}
--- src/test/java/testLayer/LoginPageTest.java
+++ src/test/java/testLayer/LoginPageTest.java
@@ -3,10 +3,13 @@ package testLayer;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class LoginPageTest {
+import Pages.LoginPage;
+
+public class LoginPageTest extends LoginPage{
 	
 	WebDriver driver=new ChromeDriver();
 	
+	LoginPage loginPage =new LoginPage();
 	
-
+	
 }
