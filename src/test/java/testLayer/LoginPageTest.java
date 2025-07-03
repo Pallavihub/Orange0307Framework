1413bb1a5491b0c701d3138b1a20bb153c60d5b0
user <user@LAPTOP-6I041NL7>
2025-07-03 11:58:18 +0530

    Framework is developed

---
+++ src/test/java/testLayer/LoginPageTest.java
@@ -0,0 +1,12 @@
+package testLayer;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LoginPageTest {
+	
+	WebDriver driver=new ChromeDriver();
+	
+	
+
+}
